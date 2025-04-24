a08ca1d768e7aad42ee20121dc017f55830ce55f
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2025-04-24 12:11:18 +0530

    Setup MUI Theme

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,14 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
